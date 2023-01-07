f81f8dfeca2f4d1f66f579c892853caf25be0685
I'm ÀŚHÌŚH  MÀŁEY <90534593+Ashishmaley@users.noreply.github.com>
2023-01-07 13:33:20 +0530

    Memer App

--- app/src/main/java/com/example/memer/MainActivity.kt
+++ app/src/main/java/com/example/memer/MainActivity.kt
@@ -1,21 +1,14 @@
 package com.example.memer
 
 import android.content.Intent
-import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.android.volley.Request
-import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
 import com.bumptech.glide.Glide
-import com.bumptech.glide.load.DataSource
-import com.bumptech.glide.load.engine.GlideException
-import com.bumptech.glide.request.RequestListener
-import com.bumptech.glide.request.target.Target
-import com.google.android.gms.ads.AdView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -26,21 +19,17 @@ class MainActivity : AppCompatActivity() {
         loadmeme()
     }
     private fun loadmeme(){
-
-        progressbar.visibility = View.VISIBLE
-
         val queue = Volley.newRequestQueue(this)
         val url = " https://meme-api.com/gimme"
 
         val jsonObjectRequest = JsonObjectRequest (
             /* method = */ Request.Method.GET, /* url = */ url, /* jsonRequest = */ null,
-            /* listener = */ Response.Listener { response ->
+            /* listener = */ { response ->
                 currentImageurl = response.getString("url")
-
 
                 Glide.with(this).load(currentImageurl).into(imageashish) },
 
-            /* errorListener = */ Response.ErrorListener {
+            {
                 Toast.makeText(this, "something went wrong", Toast.LENGTH_SHORT).show()
             })
 
